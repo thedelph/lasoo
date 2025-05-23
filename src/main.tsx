bc39a440d4b11005263b7864d5d8ea9a6fc97b3c
thedelph <chrishide87@gmail.com>
2025-05-23 10:00:24 +0100

    feat: add Vercel Analytics integration to React app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.supabase' // Using the Supabase version of App
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
